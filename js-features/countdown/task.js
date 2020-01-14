04be950577552d9dacbcd10c7f11bcff47f6080a
Kate Pronina <kateadm@narod.ru>
2020-01-14 12:04:39 +0300

    Исправленное домашнее задание

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,10 +1,13 @@
 const timer = document.getElementById("timer");
 
-let addTimer = setInterval(() => {
+let addTimer = () => {
   if (timer.textContent == 0) {
-    alert("Вы победили в конкурсе"); 
+    alert("Вы победили в конкурсе");
+    clearInterval(intervalID);
   } 
   else {
     timer.textContent -= 1;
   }
-}, 1000);
+}
+
+var intervalID = setInterval(addTimer, 1000);
